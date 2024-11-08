a2d4e92a4ee23a7c9af27d9b8bcc4f0b8f235205
JethroNatividad <natividad.jet@gmail.com>
2024-11-09 02:09:36 +0800

    feat: create test email

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UsersController;
+use App\Mail\MyTestEmail;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -12,5 +14,12 @@ Route::get('/', function () {
 })->middleware(['auth'])->name('home');
 
 Route::resource('users', UsersController::class)->only(['index'])->middleware(['auth']);
+
+// Route::get('/testroute', function () {
+//     $name = "Funny Coder";
+
+//     // The email sending is done using the to method on the Mail facade
+//     Mail::to('natividad.jet@gmail.com')->send(new MyTestEmail());
+// });
 
 require __DIR__ . '/auth.php';
