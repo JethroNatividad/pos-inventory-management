abffdfb3d51a55e672df5c1d61f19551e21d6bd9
JethroNatividad <natividad.jet@gmail.com>
2024-11-09 23:32:09 +0800

    fix: create user button

--- resources/js/Pages/Users/data-table.tsx
+++ resources/js/Pages/Users/data-table.tsx
@@ -99,7 +99,7 @@ export function DataTable<TData, TValue>({
                     </div>
                 </div>
 
-                <Button>
+                <Button asChild>
                     <Link href={route("users.create")}>Create User</Link>
                 </Button>
             </div>
