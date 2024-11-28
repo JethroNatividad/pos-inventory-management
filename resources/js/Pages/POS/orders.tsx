27abd29911372e7b06f8b33ea5227de80f45a011
JethroNatividad <natividad.jet@gmail.com>
2024-11-29 00:54:34 +0800

    feat: add checkout button

--- resources/js/Pages/POS/orders.tsx
+++ resources/js/Pages/POS/orders.tsx
@@ -8,19 +8,20 @@ import { Trash } from "lucide-react";
 const Orders = () => {
     const { orders, clearOrders } = useOrder();
     return (
-        <div className="border rounded-lg w-full h-screen max-h-[90vh] overflow-y-scroll relative">
-            <div className="">
-                <div className="sticky top-0 left-0 bg-background w-full p-4 shadow-sm flex justify-between">
-                    <h2 className="text-2xl">Orders</h2>
-                    <Button variant="destructive" onClick={clearOrders}>
-                        <Trash />
-                    </Button>
-                </div>
-                <div className="flex flex-col space-y-4 p-4">
-                    {orders.map((order) => (
-                        <Order key={order.id} {...order} />
-                    ))}
-                </div>
+        <div className="border rounded-lg w-full h-screen max-h-[90vh] overflow-y-scroll relative flex flex-col">
+            <div className="sticky top-0 left-0 bg-background w-full p-4 border-b flex justify-between items-center">
+                <h2 className="text-2xl">Orders</h2>
+                <Button variant="destructive" onClick={clearOrders}>
+                    <Trash />
+                </Button>
+            </div>
+            <div className="flex flex-col space-y-4 p-4 flex-1">
+                {orders.map((order) => (
+                    <Order key={order.id} {...order} />
+                ))}
+            </div>
+            <div className="sticky bg-background bottom-0 left-0 w-full p-4 border-t">
+                <Button className="w-full">Checkout</Button>
             </div>
         </div>
     );
