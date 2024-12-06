ff5a52541074168249d56209aac084c38447d762
JethroNatividad <natividad.jet@gmail.com>
2024-12-06 11:56:58 +0800

    feat: add no recipes on pos

--- resources/js/Pages/POS/items.tsx
+++ resources/js/Pages/POS/items.tsx
@@ -9,6 +9,9 @@ type Props = {
 const Items = ({ recipes }: Props) => {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+            {recipes.length === 0 && (
+                <div className="text-center">No recipes found.</div>
+            )}
             {recipes.map((recipe) => (
                 <Item key={recipe.id} {...recipe} />
             ))}
