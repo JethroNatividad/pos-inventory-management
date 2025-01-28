de5ad28350b06edc151069e398fd199b3f06b6b9
JethroNatividad <natividad.jet@gmail.com>
2025-01-29 02:48:29 +0800

    feat: add sonner

--- resources/js/Layouts/Layout.tsx
+++ resources/js/Layouts/Layout.tsx
@@ -4,6 +4,7 @@ import {
     SidebarTrigger,
     useSidebar,
 } from "@/Components/ui/sidebar";
+import { Toaster } from "@/Components/ui/sonner";
 import { usePage } from "@inertiajs/react";
 import { PropsWithChildren } from "react";
 import Cookies from "js-cookie";
@@ -21,6 +22,7 @@ export default function Layout({ children }: PropsWithChildren) {
                 </div>
                 <div className="px-6 pt-16 pb-4">{children}</div>
             </main>
+            <Toaster />
         </SidebarProvider>
     );
 }
