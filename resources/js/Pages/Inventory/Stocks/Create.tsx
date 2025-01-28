b172fe26dde3ff78914e11fda81e35b746d0dc42
JethroNatividad <natividad.jet@gmail.com>
2025-01-28 08:06:48 +0800

    feat: auto batch label

--- resources/js/Pages/Inventory/Stocks/Create.tsx
+++ resources/js/Pages/Inventory/Stocks/Create.tsx
@@ -30,7 +30,7 @@ type Props = {
 
 const AddStock = ({ stockEntry }: Props) => {
     const { data, setData, post, processing, errors, reset } = useForm({
-        batch_label: "",
+        batch_label: `Batch ${format(new Date(), "yyyy-MM-dd")}`,
         quantity: "",
         price: "",
         unit: units[stockEntry.type][0],
