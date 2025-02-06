dcad0ecb878b1bec1d88b03d9fcd35e506889339
JethroNatividad <natividad.jet@gmail.com>
2025-02-06 21:29:07 +0800

    feat: implement caching

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,6 +8,7 @@ use App\Models\Serving;
 use App\Models\StockActivityLogs;
 use DateTime;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -18,12 +19,16 @@ class OrderController extends Controller
         $from = $request->input('from') ? new DateTime($request->input('from')) : null;
         $to = $request->input('to') ? new DateTime($request->input('to')) : null;
 
-        $orders = Order::with(['items.serving.recipe'])->get();
+        // Create cache key based on date parameters
+        $cacheKey = 'order_stats_' . ($from?->format('Y-m-d') ?? 'all') . '_' . ($to?->format('Y-m-d') ?? 'all');
 
-
-        return response()->json(
-            OrderStatsDTO::fromOrders($orders, $from, $to)->toArray()
-        );
+        // Get or set cached data (1 hours)
+        return Cache::remember($cacheKey, 60 * 60 * 1, function () use ($from, $to) {
+            $orders = Order::with(['items.serving.recipe'])->get();
+            return response()->json(
+                OrderStatsDTO::fromOrders($orders, $from, $to)->toArray()
+            );
+        });
     }
 
     /**
