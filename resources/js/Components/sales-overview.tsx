9a2cb99f1f2baff84097660fd22f22cb90d886c5
JethroNatividad <natividad.jet@gmail.com>
2025-02-12 19:48:28 +0800

    feat: make sales overview responsive

--- resources/js/Components/sales-overview.tsx
+++ resources/js/Components/sales-overview.tsx
@@ -292,7 +292,7 @@ const SalesOverview = () => {
                     )}
                 </div>
                 <div>
-                    <div className="grid grid-cols-3 gap-4 mb-6">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-6">
                         <StatCard
                             title="Total Orders"
                             value={
