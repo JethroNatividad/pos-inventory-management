6265961ee22ad21b668615194c05c5943616380f
JethroNatividad <natividad.jet@gmail.com>
2025-03-17 04:42:59 +0800

    feat: create addon migration and model

---
+++ app/Models/AddOns.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AddOns extends Model
+{
+    protected $fillable = [
+        'order_item_id',
+        'stock_entry_id',
+        'quantity',
+        'price',
+    ];
+
+    public function orderItem()
+    {
+        return $this->belongsTo(OrderItem::class);
+    }
+
+    public function stockEntry()
+    {
+        return $this->belongsTo(StockEntry::class);
+    }
+}
