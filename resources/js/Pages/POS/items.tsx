c64e3659b95fae32fde9d3204b16bd032171cf9d
JethroNatividad <natividad.jet@gmail.com>
2025-05-04 20:45:15 +0800

    add search to pos

--- resources/js/Pages/POS/items.tsx
+++ resources/js/Pages/POS/items.tsx
@@ -1,19 +1,39 @@
 import { Recipe } from "@/types";
 import Item from "./item";
+import { useState, useMemo } from "react";
+import { Input } from "@/Components/ui/input";
+import { Button } from "@/Components/ui/button";
 
 type Props = {
     recipes: Recipe[];
 };
 
 const Items = ({ recipes }: Props) => {
+    const [query, setQuery] = useState("");
+
+    const filtered = useMemo(() => {
+        const lower = query.toLowerCase();
+        return recipes.filter((r) => r.name.toLowerCase().includes(lower));
+    }, [query, recipes]);
+
+    const clearSearch = () => setQuery("");
+
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
-            {recipes.length === 0 && (
-                <div className="text-center">No recipes found.</div>
-            )}
-            {recipes.map((recipe) => (
-                <Item key={recipe.id} recipe={recipe} />
-            ))}
+        <div>
+            <div className="mb-2 flex space-x-2">
+                <Input
+                    onChange={(e) => setQuery(e.currentTarget.value)}
+                    value={query}
+                    placeholder="Search.."
+                />
+                <Button onClick={clearSearch}>Clear</Button>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+                {filtered.length === 0 && <div>No recipes found.</div>}
+                {filtered.map((recipe) => (
+                    <Item key={recipe.id} recipe={recipe} />
+                ))}
+            </div>
         </div>
     );
 };
